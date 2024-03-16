f77461f81d8c8a427c071e377b0900211e423ece
tovDmitrij <dmxikka@gmail.com>
2024-03-16 21:41:09 +0300

    Патч -Переход от Ocelot к NGINX.

--- docker.bash
+++ docker.bash
@@ -5,7 +5,6 @@ docker compose down;
 docker image rm keyboards-api_main;
 docker image rm keyboards-api_email;
 docker image rm keyboards-api_preview;
-docker image rm keyboards-api_proxy;
 docker compose build;
 docker compose up -d;
 #docker image prune -a -f;
