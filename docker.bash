e74ee57328d9bb4271284a1ba8db9ed48378bd73
tovDmitrij <dmxikka@gmail.com>
2024-04-15 17:02:17 +0300

    Обновление баш скрипта

--- docker.bash
+++ docker.bash
@@ -5,6 +5,7 @@ docker compose down;
 docker image rm keyboards-api_main;
 docker image rm keyboards-api_email;
 docker image rm keyboards-api_stats;
+docker image rm keyboards-client;
 docker compose build;
 docker compose up -d;
 echo ~~uwu~~;
