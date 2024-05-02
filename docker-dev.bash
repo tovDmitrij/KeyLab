d50ce220fe7ceef02ecdcc4e26966803f10847c1
tovDmitrij <dmxikka@gmail.com>
2024-05-02 14:10:21 +0300

    Баш

--- docker-dev.bash
+++ docker-dev.bash
@@ -6,6 +6,6 @@ docker image rm keyboards-dev-api_keyboards;
 docker image rm keyboards-dev-api_users;
 docker image rm keyboards-dev-api_email;
 docker image rm keyboards-dev-api_stats;
-docker compose -f compose-dev.yaml build;
+docker compose -f compose-dev.yaml -p 'keyboards-dev' build;
 docker compose -f compose-dev.yaml -p 'keyboards-dev' up -d;
 echo ~~uwu~~;
--- docker-production.bash
+++ docker-production.bash
@@ -6,6 +6,6 @@ docker image rm keyboards-production-api_keyboards;
 docker image rm keyboards-production-api_users;
 docker image rm keyboards-production-api_email;
 docker image rm keyboards-production-api_stats;
-docker compose -f compose-production.yaml build;
+docker compose -f compose-production.yaml -p 'keyboards-production' build;
 docker compose -f compose-production.yaml -p 'keyboards-production' up -d;
 echo ~~uwu~~;
