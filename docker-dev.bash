ba9982e76a1ee04980ff2fff79d36a18547cc851
tovDmitrij <dmxikka@gmail.com>
2024-05-02 14:11:02 +0300

    Баш

--- docker-dev.bash
+++ docker-dev.bash
@@ -2,11 +2,11 @@
 
 docker compose -p keyboards-dev stop;
 docker compose -p keyboards-dev down;
-docker image rm keyboards-dev-api_client;
+docker image rm keyboards-dev-client;
 docker image rm keyboards-dev-api_keyboards;
 docker image rm keyboards-dev-api_users;
 docker image rm keyboards-dev-api_email;
 docker image rm keyboards-dev-api_stats;
-docker compose -f compose-dev.yaml build;
+docker compose -f compose-dev.yaml -p 'keyboards-dev' build;
 docker compose -f compose-dev.yaml -p 'keyboards-dev' up -d;
 echo ~~uwu~~;
--- docker-production.bash
+++ docker-production.bash
@@ -2,11 +2,11 @@
 
 docker compose -p keyboards-production stop;
 docker compose -p keyboards-production down;
-docker image rm keyboards-production-api_client;
+docker image rm keyboards-production-client;
 docker image rm keyboards-production-api_keyboards;
 docker image rm keyboards-production-api_users;
 docker image rm keyboards-production-api_email;
 docker image rm keyboards-production-api_stats;
-docker compose -f compose-production.yaml build;
+docker compose -f compose-production.yaml -p 'keyboards-production' build;
 docker compose -f compose-production.yaml -p 'keyboards-production' up -d;
 echo ~~uwu~~;
