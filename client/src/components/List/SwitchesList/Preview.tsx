c32feb7f79ffbf9cba89a72a780fa4fa71d3dfb0
M1wk4 <nikolay.laz10@yandex.ru>
2024-06-02 18:49:32 +0300

    fixes

--- client/src/components/List/SwitchesList/Preview.tsx
+++ client/src/components/List/SwitchesList/Preview.tsx
@@ -1,10 +1,10 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useLazyGetSwitchPreviewQuery } from "../../../services/switchesService";
 import { useLazyGetBoxPreviewQuery } from "../../../services/boxesService";
 import { useLazyGetKitPreviewQuery } from "../../../services/kitsService";
 import { useLazyGetKeyBoardPreviewQuery } from "../../../services/keyboardService";
 
-type props = {
+type Props = {
   id?: string;
   type?: string;
   width?: number;
@@ -12,44 +12,46 @@ type props = {
   scale?: number;
 };
 
-const Preview: FC<props> = ({ width, height, type, id, scale}) => {
+const Preview: FC<Props> = ({ width, height, type, id, scale }) => {
   const [previewImage, setPreviewImage] = useState<string | undefined>(undefined);
-  const [switchPreview] = useLazyGetSwitchPreviewQuery();
-  const [boxPreview] = useLazyGetBoxPreviewQuery();
-  const [kitPreview] = useLazyGetKitPreviewQuery();
-  const [keyBoardPreview] = useLazyGetKeyBoardPreviewQuery();
-
+  const [getSwitchPreview] = useLazyGetSwitchPreviewQuery();
+  const [getBoxPreview] = useLazyGetBoxPreviewQuery();
+  const [getKitPreview] = useLazyGetKitPreviewQuery();
+  const [getKeyboardPreview] = useLazyGetKeyBoardPreviewQuery();
 
   useEffect(() => {
-    if (!id) return;
-    if (type === "switch") {
-      switchPreview(id)
-      .unwrap()
-      .then((data) => setPreviewImage(data.previewBase64));
-    }
-    if (type === "box") {
-      boxPreview(id)
-      .unwrap()
-      .then((data) => setPreviewImage(data.previewBase64));
-    }
-    if (type === "kit") {
-      kitPreview(id)
-      .unwrap()
-      .then((data) => {setPreviewImage(data.previewBase64)})
-    }
-    
-    if (type === "keyboard") {
-      keyBoardPreview(id)
-      .unwrap()
-      .then((data) => {setPreviewImage(data.previewBase64)})
-    }
+    if (!id || !type) return;
 
-  }, [])
-  
+    const fetchPreview = async () => {
+      let data;
+      if (type === "switch") {
+        data = await getSwitchPreview(id).unwrap();
+      } else if (type === "box") {
+        data = await getBoxPreview(id).unwrap();
+      } else if (type === "kit") {
+        data = await getKitPreview(id).unwrap();
+      } else if (type === "keyboard") {
+        data = await getKeyboardPreview(id).unwrap();
+      }
+
+      if (data) {
+        setPreviewImage(data.previewBase64);
+      }
+    };
+
+    fetchPreview();
+  }, [id, type, getSwitchPreview, getBoxPreview, getKitPreview, getKeyboardPreview]);
+
   return (
-    {previewImage} && <img src={`data:image/webp;base64,${previewImage}`} style={{
-      transform: "scale(1.1, 1.1)",
-      }} width={width} height={height} draggable="false" />
+    previewImage && (
+      <img
+        src={`data:image/webp;base64,${previewImage}`}
+        style={{ transform: `scale(${scale || 1.1})` }}
+        width={width}
+        height={height}
+        draggable="false"
+      />
+    )
   );
 };
 
